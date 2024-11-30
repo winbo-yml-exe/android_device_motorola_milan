eee9cbb8d60b90cfddb0cb0b09fe325b26eadaa6
winbo.yml <winbo.yml@proton.me>
2024-11-30 20:38:56 +0100

    milan: Inherit TWRP vendor instead of omni onme

--- twrp_milan.mk
+++ twrp_milan.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from milan device
 $(call inherit-product, device/motorola/milan/device.mk)
